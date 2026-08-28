f4733bb471f53d9d12b3104f3b8c4c18988d3eee
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2026-08-28 23:28:58 +0500

    added feature that will now shows total task of loggin user with projects

--- server/controllers/dashboardContoller.js
+++ server/controllers/dashboardContoller.js
@@ -1,16 +1,29 @@
 const Project = require("../models/Project");
+const Task = require("../models/Task");
 
 const getDashboardStatistics = async (req, res) => {
   try {
-    //count proeject where loggin user is member
-    const totalProjects = await Project.countDocuments({
+    //find all project accessible to loggin user
+    const projects = await Project.find({
       members: req.user._id,
+    }).select("_id");
+
+    //get only project IDs
+    const projectIds = projects.map(project => project._id);
+
+    //count all task belonging to user accessible projects
+    const totalTasks = await Task.countDocuments({
+      project: {$in: projectIds},
     });
+
+    //count total accesible projects
+    const totalProjects = projects.length;
 
     return res.status(200).json({
       success: true,
       statistics: {
         totalProjects,
+        totalTasks,
       },
     });
   } catch (error) {
@@ -23,6 +36,6 @@ const getDashboardStatistics = async (req, res) => {
   }
 };
 
-module.exports={
-    getDashboardStatistics
-}
+module.exports = {
+  getDashboardStatistics,
+};
