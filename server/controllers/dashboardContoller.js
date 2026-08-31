84f62eca7bbbb60c4d2c17ba87c573281e21997e
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2026-08-31 13:50:14 +0500

    feat: enhance dashboard statistics to include task counts by status and assigned tasks

--- server/controllers/dashboardContoller.js
+++ server/controllers/dashboardContoller.js
@@ -3,6 +3,10 @@ const Task = require("../models/Task");
 
 const getDashboardStatistics = async (req, res) => {
   try {
+    //count projects accessible to loggin user
+    const totalProjects = await Project.countDocuments({
+      members: req.user._id,
+    });
     //find all project accessible to loggin user
     const projects = await Project.find({
       members: req.user._id,
@@ -16,14 +20,37 @@ const getDashboardStatistics = async (req, res) => {
       project: {$in: projectIds},
     });
 
-    //count total accesible projects
-    const totalProjects = projects.length;
+    //count todo task inside accessible projects
+    const todoTasks = await Task.countDocuments({
+      project: {$in: projectIds},
+      status: "todo",
+    });
+
+    //count in-progress tasks
+    const inProgressTasks = await Task.countDocuments({
+      project: {$in: projectIds},
+      status: "in-progress",
+    });
+
+    //cuount  completed tasks
+    const completedTasks = await Task.countDocuments({
+      project: {$in: projectIds},
+      status: "completed",
+    });
+
+    const myAssignedTasks = await Task.countDocuments({
+      assignrdTo: req.user._id,
+    });
 
     return res.status(200).json({
       success: true,
       statistics: {
         totalProjects,
         totalTasks,
+        todoTasks,
+        inProgressTasks,
+        completedTasks,
+        myAssignedTasks
       },
     });
   } catch (error) {
